ca53dd3397ce62f43237e6af645cd1855499d1dc
Jacob Beal <jb567@2793307.no-reply.drupal.org>
2014-01-15 14:00:11 +1300

    fixed countdown issue by making jQuery load after the document has loaded

--- countdown.js
+++ countdown.js
@@ -1,7 +1,8 @@
-(function ($) {
+jQuery(document).ready(function ($) {
   var currentdate = new Date();
   var now = Math.round(new Date().getTime()/1000.0);
   var beer_o_clock = Drupal.settings.beer_o_clock.boc_timer;
+  console.log(beer_o_clock);
   var diff = beer_o_clock - now;
   var seconds_min = Math.floor(diff / 60);
   var minutes = seconds_min % 60;
@@ -9,7 +10,6 @@
   var hours = hour_min % 24;
   var hour_day = Math.floor(hour_min / 24);
   var days = hour_day % 365;
-	
 	//http://stackoverflow.com/questions/1267283/how-can-i-create-a-zerofilled-value-using-javascript
 	function zeroFill( number, width )
 	{
@@ -20,14 +20,13 @@
   }
   return number + ""; // always return a string
 	}
-	
 	setInterval(function () {
 		var seconds = diff % 60;
-		$("#boc_timer").text(zeroFill(days, 2)+":"+zeroFill(hours,2)+":"+zeroFill(minutes,2)+":"+zeroFill(seconds,2));	
+		$("#boc_timer").text(zeroFill(days, 2)+":"+zeroFill(hours,2)+":"+zeroFill(minutes,2)+":"+zeroFill(seconds,2));
 	console.log(zeroFill(days, 2)+":"+zeroFill(hours,2)+":"+zeroFill(minutes,2)+":"+zeroFill(seconds,2));
 		diff--;
 	}, 1000);
 
-})(jQuery);
+});
 
 
